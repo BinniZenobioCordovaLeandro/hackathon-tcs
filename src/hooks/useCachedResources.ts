c16889ca41adb9110a7cf3b470b9d0b4274edcaf
Binni Zenobio Cordova Leandro <binni.2000.cordova@gmail.com>
2021-01-10 11:56:39 -0500

    expo build:web

--- src/hooks/useCachedResources.ts
+++ src/hooks/useCachedResources.ts
@@ -15,16 +15,16 @@ export default function useCachedResources () {
         // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
-          'OpenSans-Bold': require('../assets/fonts/OpenSans-Bold.ttf'),
-          'OpenSans-BoldItalic': require('../assets/fonts/OpenSans-BoldItalic.ttf'),
-          'OpenSans-ExtraBold': require('../assets/fonts/OpenSans-ExtraBold.ttf'),
-          'OpenSans-ExtraBoldItalic': require('../assets/fonts/OpenSans-ExtraBoldItalic.ttf'),
-          'OpenSans-Italic': require('../assets/fonts/OpenSans-Italic.ttf'),
-          'OpenSans-Light': require('../assets/fonts/OpenSans-Light.ttf'),
-          'OpenSans-LightItalic': require('../assets/fonts/OpenSans-LightItalic.ttf'),
-          'OpenSans-Regular': require('../assets/fonts/OpenSans-Regular.ttf'),
-          'OpenSans-Semibold': require('../assets/fonts/OpenSans-Semibold.ttf'),
-          'OpenSans-SemiboldItalic': require('../assets/fonts/OpenSans-SemiboldItalic.ttf')
+          'OpenSans-Bold': require('../../assets/fonts/OpenSans-Bold.ttf'),
+          'OpenSans-BoldItalic': require('../../assets/fonts/OpenSans-BoldItalic.ttf'),
+          'OpenSans-ExtraBold': require('../../assets/fonts/OpenSans-ExtraBold.ttf'),
+          'OpenSans-ExtraBoldItalic': require('../../assets/fonts/OpenSans-ExtraBoldItalic.ttf'),
+          'OpenSans-Italic': require('../../assets/fonts/OpenSans-Italic.ttf'),
+          'OpenSans-Light': require('../../assets/fonts/OpenSans-Light.ttf'),
+          'OpenSans-LightItalic': require('../../assets/fonts/OpenSans-LightItalic.ttf'),
+          'OpenSans-Regular': require('../../assets/fonts/OpenSans-Regular.ttf'),
+          'OpenSans-Semibold': require('../../assets/fonts/OpenSans-Semibold.ttf'),
+          'OpenSans-SemiboldItalic': require('../../assets/fonts/OpenSans-SemiboldItalic.ttf')
         })
       } catch (e) {
         // We might want to provide this error information to an error reporting service
